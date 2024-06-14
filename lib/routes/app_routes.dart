b9a12e14a6878e2b9f05b25824926adb212ca6a4
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-06-14 21:42:51 +0530

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:auth_nodejs_backend/screens/auth/login.dart';
 import 'package:auth_nodejs_backend/screens/auth/signup.dart';
 
+
 class AppRoutes {
   static const String loginRoute = '/login';
   static const String signupRoute = '/signup';
