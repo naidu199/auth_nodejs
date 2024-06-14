f2e39344d5b0fd9578f8d5ced4160726242d638c
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-06-14 21:48:15 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Home page"),
+        child: Text("Home page Demo"),
       ),
     );
   }
