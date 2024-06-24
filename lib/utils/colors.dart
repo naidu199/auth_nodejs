76326d8e69b26f257179ccf630519e41464d5b9a
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-06-24 13:42:49 +0530

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
